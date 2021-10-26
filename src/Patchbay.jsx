d7b7588fa61351eea8c3b12db9581fe7aa82f645
carlocattano <cattanoaudioproduction@gmail.com>
2021-10-26 15:26:03 +0200

    Fixed issue 1 by using [index] = instead of array.push()

--- src/Patchbay.jsx
+++ src/Patchbay.jsx
@@ -3,58 +3,52 @@ import JsonData from './test_data.json'
 
 import ReactFlow, {
 	addEdge,
-	removeElements,
 	Background,
   } from 'react-flow-renderer';
 
-const ports= []
+const ports = []
 
 function RenderPorts(){
-	const DisplayData=JsonData.map(
-		(info) => {
+	const DisplayData=JsonData.map( (info) => {
 			if (info.type === 'input') {
-				return(
-					ports.push({
+				ports[info.id]={
 						id: info.name, type: 'input', data: { label: info.name },
-						position: { x: 100, y: 60 * info.id }, animated: true, arrowHeadType: 'arrowclosed',
+						position: { x: 100, y: 60 * info.id }, animated: true, 
 						sourcePosition: 'right'
-					})
-				)
+				}
 			}
-			else if (info.type ===	 'output') {
-				return(
-					ports.push({
+			if (info.type === 'output') {
+				ports[info.id]={
 						id: info.name, type: 'output', data: { label: info.name },
-						position: { x: 600, y:( 50 * info.id)-200 }, animated: true, arrowHeadType: 'arrowclosed',
+						position: { x: 600, y:( 50 * info.id)-200 }, animated: true, 
 						targetPosition: 'left'
-					})
-				)
+				}
 			}
-		}	// info
+		},
+		//console.log(ports)
 	)
 	return(
-		<div>
-			{DisplayData}
+		<div>		
+			{DisplayData} 	
 			<div style={{ height: 800 }}>
-			<BasicFlow>	
+				<BasicFlow>	
 					<Background color="#FFFF" gap={16} />
-			</BasicFlow>
+				</BasicFlow>
 			</div>
 		</div>
 	)
 }
-const BasicFlow = () =>{
+
+function BasicFlow() {
 
 	const [elements, setElements] = useState(ports);
-	const onElementsRemove = (elementsToRemove) => setElements((els) => removeElements(elementsToRemove, els));
-	const onConnect = (params) => setElements((els) => addEdge(params, els), 	console.log("Source " ,params.source," Target ", params.target));
-
+	const onConnect = (params) => setElements((els) => addEdge(params, els), console.log("Source ", params.source, " Target ", params.target));
 
 	return (
-		<ReactFlow elements={elements} snapToGrid={true} 
-							onConnect={onConnect}onElementsRemove={onElementsRemove}>
+		<ReactFlow elements={elements} snapToGrid={true}
+			onConnect={onConnect}>
 		</ReactFlow>
-	)
+	);
 }
 
 export default RenderPorts;
